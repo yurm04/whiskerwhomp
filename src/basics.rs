666f89aa6601aabd4719eac531c829e168f33a7c
Yuraima Estevez <yuraima.estevez@shopify.com>
2024-05-29 16:30:12 -0500

    added ball game assets and loaded ball on screen

---
+++ src/basics.rs
@@ -0,0 +1,54 @@
+use bevy::{ecs::query, prelude::*};
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, (update_people, greet_people).chain());
+    }
+}
+
+fn main() {
+    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
+}
+
+fn hello_world() {
+    println!("Hello, World!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Yuraima".to_string())));
+    commands.spawn((Person, Name("Raquel".to_string())));
+    commands.spawn((Person, Name("Artie".to_string())));
+    commands.spawn((Person, Name("Dom".to_string())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello, {}", name.0)
+        }
+
+        println!("-----")
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Yuraima" {
+            name.0 = "Yurms".to_string();
+            break;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,38 @@
-use bevy::{ecs::query, prelude::*};
+use bevy::{ecs::query, prelude::*, transform::commands, window::PrimaryWindow};
 
 #[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_systems(Startup, add_people)
-            .add_systems(Update, (update_people, greet_people).chain());
-    }
-}
+pub struct Player {}
 
 fn main() {
-    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_player)
+        .add_systems(Startup, spawn_camera)
+        .run();
 }
 
-fn hello_world() {
-    println!("Hello, World!");
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("sprites/ball_blue_large.png"),
+            ..default()
+        },
+        Player {},
+    ));
 }
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Yuraima".to_string())));
-    commands.spawn((Person, Name("Raquel".to_string())));
-    commands.spawn((Person, Name("Artie".to_string())));
-}
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("Hello, {}", name.0)
-        }
-
-        println!("-----")
-    }
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Yuraima" {
-            name.0 = "Yurms".to_string();
-            break;
-        }
-    }
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
 }
