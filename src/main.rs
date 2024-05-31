0d427865d519d32b2dde997cc69913b410651993
Yuraima Estevez <yuraima.estevez@shopify.com>
2024-05-31 14:30:15 -0500

    added movement with arrow keys

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_player)
         .add_systems(Startup, spawn_camera)
+        .add_systems(Update, player_movement)
         .run();
 }
 
@@ -20,7 +21,7 @@ pub fn spawn_player(
 
     commands.spawn((
         SpriteBundle {
-            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            transform: Transform::from_xyz(window.width() / 2., window.height() / 2., 0.),
             texture: asset_server.load("sprites/ball_blue_large.png"),
             ..default()
         },
@@ -32,7 +33,27 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
     let window = window_query.get_single().unwrap();
 
     commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        transform: Transform::from_xyz(window.width() / 2., window.height() / 2., 0.),
         ..default()
     });
+}
+
+fn player_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut head_positions: Query<&mut Transform, With<Player>>,
+) {
+    for mut transform in head_positions.iter_mut() {
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            transform.translation.x -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
+            transform.translation.x += 2.;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowDown) {
+            transform.translation.y -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowUp) {
+            transform.translation.y += 2.;
+        }
+    }
 }
