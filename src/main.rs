9f4842ca9f5e70444aec9f3b8131d26ccc6a21db
Yuraima <yurm@yuraimas-mbp.lan>
2024-06-12 20:40:00 -0500

    added sprite animation
    Co-authored-by: Dominik Wilkowski <hi@dominik-wilkowski.com>

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,17 @@ use bevy::{
     window::{PrimaryWindow, Window, WindowPlugin, WindowResolution},
 };
 
+pub enum PlayerState {
+    IdleRight,
+    IdleLeft,
+    WalkingRight,
+    WalkingLeft,
+}
+
 #[derive(Component)]
-pub struct Player {}
+pub struct Player {
+    state: PlayerState,
+}
 
 #[derive(Component)]
 pub struct PlayerPosition {}
@@ -19,19 +28,85 @@ pub struct SpacePressed {
 
 const WINDOW_WIDTH: f32 = 800.0;
 const WINDOW_HEIGHT: f32 = 500.0;
+const INDEX_FIRST: usize = 33;
+const INDEX_LAST: usize = 40;
+
+#[derive(Component)]
+struct AnimationIndices {
+    first: usize,
+    last: usize,
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<(&AnimationIndices, &mut AnimationTimer, &mut TextureAtlas)>,
+) {
+    for (indices, mut timer, mut atlas) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            atlas.index = if atlas.index == indices.last {
+                indices.first
+            } else {
+                atlas.index + 1
+            };
+        }
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let texture = asset_server.load("sprites/cat_sprite.png");
+    let layout = TextureAtlasLayout::from_grid(Vec2::new(32.0, 32.0), 8, 10, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    // Use only the subset of sprites in the sheet that make up the run animation
+    let animation_indices = AnimationIndices {
+        first: INDEX_FIRST,
+        last: INDEX_LAST,
+    };
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        SpriteSheetBundle {
+            texture,
+            atlas: TextureAtlas {
+                layout: texture_atlas_layout,
+                index: animation_indices.first,
+            },
+            transform: Transform::from_scale(Vec3::splat(4.0)),
+            ..default()
+        },
+        animation_indices,
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        Player {
+            state: PlayerState::IdleRight,
+        },
+    ));
+}
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
-                resizable: false,
-                ..default()
-            }),
-            ..default()
-        }))
-        .add_systems(Startup, spawn_player)
-        .add_systems(Startup, spawn_camera)
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        // .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) // prevents blurry sprites
+        .add_systems(Startup, setup)
+        .add_systems(Update, animate_sprite)
+        // .add_systems(Startup, spawn_player)
+        // .add_systems(Startup, spawn_camera)
         .add_systems(Startup, spawn_text)
         .add_systems(Update, keyboard_control)
         .add_systems(Update, text_update_position)
@@ -47,11 +122,13 @@ pub fn spawn_player(
 
     commands.spawn((
         SpriteBundle {
-            transform: Transform::from_xyz(window.width() / 2., window.height() / 2., 0.),
+            transform: Transform::from_xyz(0., 0., 0.),
             texture: asset_server.load("sprites/ball_blue_large.png"),
             ..default()
         },
-        Player {},
+        Player {
+            state: PlayerState::IdleLeft,
+        },
     ));
 }
 
@@ -77,12 +154,12 @@ fn keyboard_control(
     if keyboard_input.pressed(KeyCode::ArrowRight) && transform.translation.x < WINDOW_WIDTH {
         transform.translation.x += 10.;
     }
-    if keyboard_input.pressed(KeyCode::ArrowDown) && transform.translation.y > 0. {
-        transform.translation.y -= 10.;
-    }
-    if keyboard_input.pressed(KeyCode::ArrowUp) && transform.translation.y < WINDOW_HEIGHT {
-        transform.translation.y += 10.;
-    }
+    // if keyboard_input.pressed(KeyCode::ArrowDown) && transform.translation.y > 0. {
+    //     transform.translation.y -= 10.;
+    // }
+    // if keyboard_input.pressed(KeyCode::ArrowUp) && transform.translation.y < WINDOW_HEIGHT {
+    //     transform.translation.y += 10.;
+    // }
 
     if keyboard_input.just_pressed(KeyCode::Space) {
         let mut text = space_text.get_single_mut().unwrap();
