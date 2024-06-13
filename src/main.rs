1d3283e81c8236f4b71ec679e4100c56e900dbf6
Yuraima <yurm@yuraimas-mbp.lan>
2024-06-12 21:59:01 -0500

    update and display player state

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use bevy::{
     window::{PrimaryWindow, Window, WindowPlugin, WindowResolution},
 };
 
-pub enum PlayerState {
+#[derive(Debug)]
+pub enum State {
     IdleRight,
     IdleLeft,
     WalkingRight,
@@ -14,9 +15,10 @@ pub enum PlayerState {
 }
 
 #[derive(Component)]
-pub struct Player {
-    state: PlayerState,
-}
+pub struct Player {}
+
+#[derive(Component)]
+pub struct PlayerState(State);
 
 #[derive(Component)]
 pub struct PlayerPosition {}
@@ -26,10 +28,31 @@ pub struct SpacePressed {
     times: usize,
 }
 
+// window size
 const WINDOW_WIDTH: f32 = 800.0;
 const WINDOW_HEIGHT: f32 = 500.0;
-const INDEX_FIRST: usize = 33;
-const INDEX_LAST: usize = 40;
+
+// sprite sheet indices
+const IDLE_FIRST: usize = 0;
+const IDLE_LAST: usize = 3;
+const IDLE2_FIRST: usize = 8;
+const IDLE2_LAST: usize = 11;
+const GROOM_FIRST: usize = 16;
+const GROOM_LAST: usize = 19;
+const GROOM2_FIRST: usize = 24;
+const GROOM2_LAST: usize = 27;
+const WALK_FIRST: usize = 32;
+const WALK_LAST: usize = 39;
+const RUN_FIRST: usize = 40;
+const RUN_LAST: usize = 47;
+const SLEEP_FIRST: usize = 48;
+const SLEEP_LAST: usize = 51;
+const PAT_FIRST: usize = 56;
+const PAT_LAST: usize = 61;
+const JUMP_FIRST: usize = 64;
+const JUMP_LAST: usize = 70;
+const SCARE_FIRST: usize = 72;
+const SCARE_LAST: usize = 79;
 
 #[derive(Component)]
 struct AnimationIndices {
@@ -40,53 +63,8 @@ struct AnimationIndices {
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
 
-fn animate_sprite(
-    time: Res<Time>,
-    mut query: Query<(&AnimationIndices, &mut AnimationTimer, &mut TextureAtlas)>,
-) {
-    for (indices, mut timer, mut atlas) in &mut query {
-        timer.tick(time.delta());
-        if timer.just_finished() {
-            atlas.index = if atlas.index == indices.last {
-                indices.first
-            } else {
-                atlas.index + 1
-            };
-        }
-    }
-}
-
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-) {
-    let texture = asset_server.load("sprites/cat_sprite.png");
-    let layout = TextureAtlasLayout::from_grid(Vec2::new(32.0, 32.0), 8, 10, None, None);
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
-    // Use only the subset of sprites in the sheet that make up the run animation
-    let animation_indices = AnimationIndices {
-        first: INDEX_FIRST,
-        last: INDEX_LAST,
-    };
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn((
-        SpriteSheetBundle {
-            texture,
-            atlas: TextureAtlas {
-                layout: texture_atlas_layout,
-                index: animation_indices.first,
-            },
-            transform: Transform::from_scale(Vec3::splat(4.0)),
-            ..default()
-        },
-        animation_indices,
-        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
-        Player {
-            state: PlayerState::IdleRight,
-        },
-    ));
-}
+#[derive(Component)]
+pub struct PlayerStateText {}
 
 fn main() {
     App::new()
@@ -110,7 +88,39 @@ fn main() {
         .add_systems(Startup, spawn_text)
         .add_systems(Update, keyboard_control)
         .add_systems(Update, text_update_position)
+        .add_systems(Update, text_update_player_state)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let texture = asset_server.load("sprites/cat_sprite.png");
+    let layout = TextureAtlasLayout::from_grid(Vec2::new(32.0, 32.0), 8, 10, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    // Use only the subset of sprites in the sheet that make up the run animation
+    let animation_indices = AnimationIndices {
+        first: IDLE_FIRST,
+        last: IDLE_LAST,
+    };
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        SpriteSheetBundle {
+            texture,
+            atlas: TextureAtlas {
+                layout: texture_atlas_layout,
+                index: animation_indices.first,
+            },
+            transform: Transform::from_scale(Vec3::splat(4.0)),
+            ..default()
+        },
+        animation_indices,
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        Player {},
+        PlayerState(State::IdleRight),
+    ));
 }
 
 pub fn spawn_player(
@@ -126,9 +136,7 @@ pub fn spawn_player(
             texture: asset_server.load("sprites/ball_blue_large.png"),
             ..default()
         },
-        Player {
-            state: PlayerState::IdleLeft,
-        },
+        Player {},
     ));
 }
 
@@ -141,18 +149,46 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
     });
 }
 
+fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<(&AnimationIndices, &mut AnimationTimer, &mut TextureAtlas)>,
+) {
+    for (indices, mut timer, mut atlas) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            atlas.index = if atlas.index == indices.last {
+                indices.first
+            } else {
+                atlas.index + 1
+            };
+        }
+    }
+}
+
 fn keyboard_control(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut player_position: Query<&mut Transform, With<Player>>,
     mut space_text: Query<(&mut Text, &mut SpacePressed), With<SpacePressed>>,
+    mut player_state: Query<&mut PlayerState, With<Player>>,
 ) {
     let mut transform = player_position.get_single_mut().unwrap();
+    let mut state = player_state.get_single_mut().unwrap();
 
     if keyboard_input.pressed(KeyCode::ArrowLeft) && transform.translation.x > 0. {
         transform.translation.x -= 10.;
+        state.0 = State::WalkingRight
     }
     if keyboard_input.pressed(KeyCode::ArrowRight) && transform.translation.x < WINDOW_WIDTH {
         transform.translation.x += 10.;
+        state.0 = State::WalkingRight
+    }
+    if keyboard_input.just_released(KeyCode::ArrowLeft) && transform.translation.x > 0. {
+        transform.translation.x -= 10.;
+        state.0 = State::IdleRight
+    }
+    if keyboard_input.just_released(KeyCode::ArrowLeft) && transform.translation.x < WINDOW_WIDTH {
+        transform.translation.x += 10.;
+        state.0 = State::IdleRight
     }
     // if keyboard_input.pressed(KeyCode::ArrowDown) && transform.translation.y > 0. {
     //     transform.translation.y -= 10.;
@@ -179,6 +215,16 @@ fn text_update_position(
     text.sections[0].value = format!("{}, {}", transform.translation.x, transform.translation.y);
 }
 
+fn text_update_player_state(
+    player_state: Query<&PlayerState, With<Player>>,
+    mut text_query: Query<&mut Text, With<PlayerStateText>>,
+) {
+    let state = player_state.get_single().unwrap();
+    let mut text = text_query.get_single_mut().unwrap();
+
+    text.sections[0].value = format!("{:?}", state.0);
+}
+
 fn spawn_text(mut commands: Commands) {
     commands.spawn((
         TextBundle::from_section("HEllo!", TextStyle::default()),
@@ -193,5 +239,15 @@ fn spawn_text(mut commands: Commands) {
             ..default()
         }),
         SpacePressed { times: 0 },
+    ));
+
+    commands.spawn((
+        TextBundle::from_section(String::new(), TextStyle::default()).with_style(Style {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(20.0),
+            right: Val::Px(5.0),
+            ..default()
+        }),
+        PlayerStateText {},
     ));
 }
