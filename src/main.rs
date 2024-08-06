b9df68e880f1c28aca10769c90ce8dcb63b930f9
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2024-08-07 08:41:37 +1000

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -20,42 +20,46 @@ const COLOR_BACKGROUND: Color = Color::srgb(0.13, 0.13, 0.23);
 const COLOR_FLOOR: Color = Color::srgb(0.45, 0.55, 0.66);
 
 fn main() {
-    App::new()
-        .insert_resource(ClearColor(COLOR_BACKGROUND))
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Bevy Platformer".to_string(),
-                resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
-                resizable: true,
-                ..Default::default()
-            }),
-            ..Default::default()
-        }))
-        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(200.0))
-        .add_plugins(RapierDebugRenderPlugin::default())
-        .add_plugins(PlatformsPlugin)
-        .add_plugins(PlayerPlugin)
-        .add_plugins(AnimationPlugin)
-        .add_systems(Startup, setup)
-        .run();
+	App::new()
+		.insert_resource(ClearColor(COLOR_BACKGROUND))
+		.add_plugins(DefaultPlugins.set(WindowPlugin {
+			primary_window: Some(Window {
+				title: "Bevy Platformer".to_string(),
+				resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+				resizable: true,
+				..Default::default()
+			}),
+			..Default::default()
+		}))
+		.add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(200.0))
+		.add_plugins(RapierDebugRenderPlugin::default())
+		.add_plugins(PlatformsPlugin)
+		.add_plugins(PlayerPlugin)
+		.add_plugins(AnimationPlugin)
+		.add_systems(Startup, setup)
+		.run();
 }
 
 fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+	commands.spawn(Camera2dBundle::default());
 
-    commands
-        .spawn(SpriteBundle {
-            sprite: Sprite {
-                color: COLOR_FLOOR,
-                ..Default::default()
-            },
-            transform: Transform {
-                translation: Vec3::new(0.0, WINDOW_BOTTOM_Y + (FLOOR_THICKNESS / 2.0), 0.0),
-                scale: Vec3::new(WINDOW_WIDTH, FLOOR_THICKNESS, 1.0),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .insert(RigidBody::Fixed)
-        .insert(Collider::cuboid(0.5, 0.5));
+	commands
+		.spawn(SpriteBundle {
+			sprite: Sprite {
+				color: COLOR_FLOOR,
+				..Default::default()
+			},
+			transform: Transform {
+				translation: Vec3::new(
+					0.0,
+					WINDOW_BOTTOM_Y + (FLOOR_THICKNESS / 2.0),
+					0.0,
+				),
+				scale: Vec3::new(WINDOW_WIDTH, FLOOR_THICKNESS, 1.0),
+				..Default::default()
+			},
+			..Default::default()
+		})
+		.insert(RigidBody::Fixed)
+		.insert(Collider::cuboid(0.5, 0.5));
 }
--- src/platforms.rs
+++ src/platforms.rs
@@ -7,42 +7,42 @@ const COLOR_PLATFORM: Color = Color::srgb(0.29, 0.31, 0.41);
 
 #[derive(Bundle)]
 struct PlatformBundle {
-    sprite_bundle: SpriteBundle,
-    body: RigidBody,
-    collider: Collider,
+	sprite_bundle: SpriteBundle,
+	body: RigidBody,
+	collider: Collider,
 }
 
 impl PlatformBundle {
-    fn new(x: f32, scale: Vec3) -> Self {
-        Self {
-            sprite_bundle: SpriteBundle {
-                sprite: Sprite {
-                    color: COLOR_PLATFORM,
-                    ..Default::default()
-                },
-                transform: Transform {
-                    translation: Vec3::new(x, WINDOW_BOTTOM_Y + (scale.y / 2.0), 0.0),
-                    scale,
-                    ..Default::default()
-                },
-                ..Default::default()
-            },
-            body: RigidBody::Fixed,
-            collider: Collider::cuboid(0.5, 0.5),
-        }
-    }
+	fn new(x: f32, scale: Vec3) -> Self {
+		Self {
+			sprite_bundle: SpriteBundle {
+				sprite: Sprite {
+					color: COLOR_PLATFORM,
+					..Default::default()
+				},
+				transform: Transform {
+					translation: Vec3::new(x, WINDOW_BOTTOM_Y + (scale.y / 2.0), 0.0),
+					scale,
+					..Default::default()
+				},
+				..Default::default()
+			},
+			body: RigidBody::Fixed,
+			collider: Collider::cuboid(0.5, 0.5),
+		}
+	}
 }
 
 pub struct PlatformsPlugin;
 
 impl Plugin for PlatformsPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
-    }
+	fn build(&self, app: &mut App) {
+		app.add_systems(Startup, setup);
+	}
 }
 
 fn setup(mut commands: Commands) {
-    commands.spawn(PlatformBundle::new(-100.0, Vec3::new(75.0, 200.0, 1.0)));
-    commands.spawn(PlatformBundle::new(100.0, Vec3::new(50.0, 350.0, 1.0)));
-    commands.spawn(PlatformBundle::new(350.0, Vec3::new(150.0, 250.0, 1.0)));
+	commands.spawn(PlatformBundle::new(-100.0, Vec3::new(75.0, 200.0, 1.0)));
+	commands.spawn(PlatformBundle::new(100.0, Vec3::new(50.0, 350.0, 1.0)));
+	commands.spawn(PlatformBundle::new(350.0, Vec3::new(150.0, 250.0, 1.0)));
 }
